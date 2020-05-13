727321e676a23e7acc5cf90ba04510e3886709d7
Badar Munir <badarvlog@gmail.com>
2020-05-13 15:59:29 +0500

    model and association

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_074718) do
+ActiveRecord::Schema.define(version: 2020_05_11_084930) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "chef_id"
+    t.integer "recipe_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
